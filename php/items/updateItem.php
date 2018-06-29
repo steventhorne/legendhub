69b3bc406d71ad0211b18c420c34d61f82d4d426
SvarturH <shalldursson@gmail.com>
2018-06-29 16:07:17 -0400

    Cleaned up item php.

--- php/items/updateItem.php
+++ php/items/updateItem.php
@@ -44,9 +44,10 @@ if (strlen(trim($mobName)) > 0) {
 unset($statArray['MobName']);
 
 $execArray = array();
-$sql = "UPDATE Items SET ModifiedOn = NOW(), ModifiedBy = :ModifiedBy, ModifiedByIP = :ModifiedByIP, ModifiedByIPForward = :ModifiedByIPForward";
-$execArray["ModifiedByIP"] = $_SERVER['REMOTE_ADDR'];
-$execArray["ModifiedByIPForward"] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+$sql = "UPDATE Items SET ModifiedOn = NOW()";
+$statArray["ModifiedByIP"] = $_SERVER['REMOTE_ADDR'];
+$statArray["ModifiedByIPForward"] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+$statArray["ModifiedBy"] = $_SESSION['Username'];
 foreach ($statArray as $key => $value)
 {
 	if ($key == "Id")
@@ -55,98 +56,11 @@ foreach ($statArray as $key => $value)
 	$sql = $sql . ", " . $cleanKey . " = :" . $cleanKey;
 	$execArray[$cleanKey] = $value;
 }
-$execArray["ModifiedBy"] = $_SESSION['Username'];
 $sql = $sql . " WHERE Id = :Id";
 $execArray["Id"] = $statArray['Id'];
 
-//echo(json_encode($sql));
-//return;
-
-
 $query = $pdo->prepare($sql);
-// $query = $pdo->prepare('UPDATE Items 
-						// set Slot = :slot,
-						    // Name = :name,
-							// Strength = :str,
-							// Mind = :min,
-							// Dexterity = :dex,
-							// Constitution = :con,
-							// Perception = :per,
-							// Spirit = :spi,
-							// Ac = :ac,
-							// Hit = :hit,
-							// Dam = :dam,
-							// Hp = :hp,
-							// Hpr = :hpr,
-							// Ma = :ma,
-							// Mar = :mar,
-							// Mv = :mv,
-							// Mvr = :mvr,
-							// Spelldam = :spelldam,
-							// Spellcrit = :spellcrit,
-							// ManaReduction = :manared,
-							// Mitigation = :miti,
-							// Accuracy = :acc,
-							// Ammo = :ammo,
-							// TwoHanded = :th,
-							// MaxDam = :maxdam,
-							// AvgDam = :avgdam,
-							// MinDam = :mindam,
-							// Parry = :parry,
-							// Holdable = :holdable,
-							// Rent = :rent,
-							// Value = :value,
-							// Weight = :weight,
-							// UniqueWear = :uniquewear,
-							// Notes = :notes,
-							// ModifiedBy = :modifiedby,
-							// ModifiedOn = NOW(),
-							// ModifiedByIP = :modifiedbyip,
-							// ModifiedByIPForward = :modifiedbyipforward,
-							// AlignRestriction = :alignrestriction
-						// WHERE Id = :id');
 $query->execute($execArray);
-//$query->execute(array(
-	// "slot" => $postdata->Slot,
-	// "name" => $postdata->Name,
-	// "str" => $postdata->Strength,
-	// "min" => $postdata->Mind,
-	// "dex" => $postdata->Dexterity,
-	// "con" => $postdata->Constitution,
-	// "per" => $postdata->Perception,
-	// "spi" => $postdata->Spirit,
-	// "ac" => $postdata->Ac,
-	// "hit" => $postdata->Hit,
-	// "dam" => $postdata->Dam,
-	// "hp" => $postdata->Hp,
-	// "hpr" => $postdata->Hpr,
-	// "ma" => $postdata->Ma,
-	// "mar" => $postdata->Mar,
-	// "mv" => $postdata->Mv,
-	// "mvr" => $postdata->Mvr,
-	// "spelldam" => $postdata->Spelldam,
-	// "spellcrit" => $postdata->Spellcrit,
-	// "manared" => $postdata->ManaReduction,
-	// "miti" => $postdata->Mitigation,
-	// "acc" => $postdata->Accuracy,
-	// "ammo" => $postdata->Ammo,
-	// "th" => $postdata->TwoHanded,
-	// "maxdam" => $postdata->MaxDam,
-	// "avgdam" => $postdata->AvgDam,
-	// "mindam" => $postdata->MinDam,
-	// "parry" => $postdata->Parry,
-	// "holdable" => $postdata->Holdable,
-	// "rent" => $postdata->Rent,
-	// "value" => $postdata->Value,
-	// "weight" => $postdata->Weight,
-	// "uniquewear" => $postdata->UniqueWear,
-	// "notes" => $postdata->Notes,
-	// "modifiedby" => $postdata->ModifiedBy,
-	// "modifiedbyip" => $_SERVER['REMOTE_ADDR'],
-	// "modifiedbyipforward" => $_SERVER['HTTP_X_FORWARDED_FOR'],
-	// "alignrestriction" => $postdata->AlignRestriction,
-	// "id" => $postdata->Id
-//));
 
 echo($pdo->lastInsertId());
 
