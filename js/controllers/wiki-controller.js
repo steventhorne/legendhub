e86ba2d44154cc3f03469c556c9986b689d438a3
SvarturH <shalldursson@gmail.com>
2018-07-03 23:45:53 -0400

    some wiki page changes, and initial css for dark/light themes

--- js/controllers/wiki-controller.js
+++ js/controllers/wiki-controller.js
@@ -1,6 +1,16 @@
-app.controller('wiki-controller', function($scope, $cookies, $http, itemConstants) {
+app.controller('wiki-controller', function($scope, $cookies, $http) {
 	$scope.initialize = function() {
-		
+		$scope.getRecentWikiPages();
+	}
+
+	$scope.getRecentWikiPages = function() {
+		$http({
+			url: '/php/wiki/getRecentWikiPages.php'
+		}).then(function succcessCallback(response) {
+			
+		}, function errorCallback(response) {
+
+		});
 	}
 
 	$scope.initialize();
