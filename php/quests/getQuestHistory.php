fc99c188d7a61df1f9e9949ccdfbfe358897cb88
SvarturH <shalldursson@gmail.com>
2018-07-08 19:56:46 -0400

    added quests.

---
+++ php/quests/getQuestHistory.php
@@ -0,0 +1,17 @@
+<?php
+header( "Access-Control-Allow-Origin: legendhub.org" );
+header( "Content-Type: application/json; charset=UTF-8" );
+
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once("$root/php/common/config.php");
+$pdo = getPDO();
+
+$postdata = json_decode(file_get_contents("php://input"));
+$id = $postdata->id;
+
+$query = $pdo->prepare('SELECT * FROM Quests_AuditTrail WHERE QuestId = ? ORDER BY ModifiedOn DESC');
+$query->execute([$id]);
+$result = $query->fetchAll(PDO::FETCH_OBJ);
+
+echo(json_encode($result))
+?>
---
+++ php/quests/updateQuest.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+
+header( "Access-Control-Allow-Origin: legendhub.org" );
+header( "Content-Type: application/json; charset=UTF-8" );
+
+$postdata = json_decode(file_get_contents("php://input"));
+
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once("$root/php/common/config.php");
+$pdo = getPDO();
+
+if (isset($_SESSION['UserId'])) {
+	$query = $pdo->prepare("SELECT Banned FROM Members WHERE Id = :id");
+	$query->execute(array("id" => $_SESSION['UserId']));
+	if ($res = $query->fetchAll(PDO::FETCH_CLASS)[0]) {
+		if ($res->Banned) {			
+			session_unset();
+			session_destroy();
+			
+			return;
+		}
+	}
+}
+else {
+	return;
+}
+
+$sql = "UPDATE Quests SET ModifiedOn = NOW(),
+			ModifiedBy = :ModifiedBy,
+			ModifiedByIP = :ModifiedByIP,
+			ModifiedByIPForward = :ModifiedByIPForward,
+			Title = :Title,
+			AreaId = :AreaId,
+			Whoises = :Whoises,
+			Content = :Content
+	WHERE Id = :Id";
+$query = $pdo->prepare($sql);
+$query->execute(array("ModifiedBy" => $_SESSION['Username'],
+			"ModifiedByIP" => $_SERVER['REMOTE_ADDR'],
+			"ModifiedByIPForward" => $_SERVER['HTTP_X_FORWARDED_FOR'],
+			"Title" => $postdata->Title,
+			"AreaId" => $postdata->AreaId,
+			"Whoises" => $postdata->Whoises,
+			"Content" => $postdata->Content,
+			"Id" => $postdata->Id));
+
+echo($pdo->lastInsertId());
+
+?>
