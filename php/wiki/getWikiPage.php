171e5dff8fd027ec81bcbbd65b4c2868ad924a1e
SvarturH <shalldursson@gmail.com>
2018-08-13 17:21:06 -0400

    Added Wiki page and Category php.

---
+++ php/wiki/getWikiPage.php
@@ -0,0 +1,19 @@
+<?php
+header( "Access-Control-Allow-Origin: legendhub.org" );
+header( "Content-Type: application/json; charset=UTF-8" );
+
+$root = realpath(getenv("DOCUMENT_ROOT"));
+require_once("$root/php/common/config.php");
+$pdo = getPDO();
+
+$postdata = json_decode(file_get_contents("php://input"));
+$id = $postdata->id;
+
+$query = $pdo->prepare('SELECT *
+						FROM WikiPages
+						WHERE Id = ?');
+$query->execute([$id]);
+$result = $query->fetchAll(PDO::FETCH_CLASS);
+
+echo(json_encode($result[0]))
+?>
