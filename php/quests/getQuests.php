c93f6b458e17f4565db29aa5f852d817c511475a
SvarturH <shalldursson@gmail.com>
2018-08-21 07:14:02 -0400

    Cleaned up mobs search page and added categories.

--- php/quests/getQuests.php
+++ php/quests/getQuests.php
@@ -8,6 +8,9 @@ $pdo = getPDO();
 
 $postdata = json_decode(file_get_contents("php://input"));
 $searchString = $postdata->searchString;
+$eraId = $postdata->eraId;
+$areaId = $postdata->areaId;
+
 if (is_null($postdata->statOnly)) {
     $statOnly = False;
 }
@@ -19,15 +22,21 @@ $sql = "SELECT Q.*, A.Name AS AreaName, A.Era AS AreaEra
         FROM Quests AS Q
             LEFT JOIN Areas AS A ON A.Id = Q.AreaId
         WHERE (:searchString IS NULL OR Q.Title LIKE :likeSearchString OR A.Name LIKE :areaLikeSearchString OR Q.Content LIKE :contentLikeSearchString OR Q.Whoises LIKE :whoisesLikeSearchString) AND
-	      (:statOnly = 0 OR Stat = 1)
+	          (:statOnly = 0 OR Stat = 1) AND
+              (:eraIdCheck <= 0 OR A.EraId = :eraId) AND
+              (:areaIdCheck <= 0 OR A.Id = :areaId)
         ORDER BY FIELD(A.Era, '', 'General', 'Ancient', 'Medieval', 'Industrial') ASC, A.Name ASC, TRIM(LEADING '...' FROM Q.Title) ASC";
 $query = $pdo->prepare($sql);
 $query->execute(['searchString' => $searchString,
                  'likeSearchString' => '%' . $searchString . '%',
-		 'areaLikeSearchString' => '%' . $searchString . '%',
+		         'areaLikeSearchString' => '%' . $searchString . '%',
                  'contentLikeSearchString' => '%' . $searchString . '%',
                  'whoisesLikeSearchString' => '%' . $searchString . '%',
-		 'statOnly' => $statOnly]);
+                 'statOnly' => $statOnly,
+                 'eraIdCheck' => $eraId,
+                 'eraId' => $eraId,
+                 'areaIdCheck' => $areaId,
+                 'areaId' => $areaId]);
 $result = $query->fetchAll(PDO::FETCH_OBJ);
 
 echo(json_encode($result))
