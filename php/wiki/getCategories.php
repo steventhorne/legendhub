7a6ddc8ca8fa844ae1f1890c8743f8f8470d9d27
SvarturH <shalldursson@gmail.com>
2018-08-23 20:57:02 -0400

    Added order number to wiki categories.

--- php/wiki/getCategories.php
+++ php/wiki/getCategories.php
@@ -6,7 +6,7 @@ $root = realpath(getenv("DOCUMENT_ROOT"));
 require_once("$root/php/common/config.php");
 $pdo = getPDO();
 
-$query = $pdo->query('SELECT * FROM Categories');
+$query = $pdo->query('SELECT * FROM Categories ORDER BY Number ASC');
 $result = $query->fetchAll(PDO::FETCH_OBJ);
 
 echo(json_encode($result))
