93172667da31c1d07040cb7cdb099c8a5a3e9b2b
SvarturH <shalldursson@gmail.com>
2018-09-03 17:29:20 -0400

    Added persistent login.

--- php/login/logout.php
+++ php/login/logout.php
@@ -4,6 +4,15 @@ session_start();
 header( "Access-Control-Allow-Origin: legendhub.org" );
 header( "Content-Type: application/json; charset=UTF-8" );
 
+$root = realpath(getenv("DOCUMENT_ROOT"));
+require_once("$root/php/common/config.php");
+$pdo = getPDO();
+
+if (isset($_SESSION['Username'])) {
+	$updateq = $pdo->prepare("DELETE FROM PersistentLogins WHERE Username = :username AND LoginIP = :ip");
+	$updateq->execute(array("username" => $_SESSION['Username'], "ip" => getenv('REMOTE_ADDR')));
+}
+
 session_unset();
 session_destroy();
 ?>
