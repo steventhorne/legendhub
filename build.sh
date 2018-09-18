db7f130a6d71e75a5dcb45129d015d34e854c8ca
SvarturH <shalldursson@gmail.com>
2018-09-18 13:44:39 +0000

    Updated build script to function properly

--- build.sh
+++ build.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+#Reminders
+read -p "Have you created your php config? [Y/n]:" configCreated
+configCreated=${configCreated:-Y}
+echo $configCreated
+if ! [[ $configCreated == "y" || $configCreated == "Y" ]]; then
+	echo "Please create your php config file using the template provided."
+	exit 1
+fi
+
 # Choose directory
 echo "Enter publish directory:"
 read -e publishPath
 
-# Move files
-if [ -d "bin" ]; then
+# Move files to bin folder to work on
+if [[ -d "bin" ]]; then
 	rm -rf bin
 fi
 mkdir bin
 
-cp -r .htaccess builder css img items maintenance.html php wiki error index.html js login.html mobs quests shared bin/
+cp -r .htaccess builder css items maintenance.html php wiki error index.html js login.html mobs quests shared bin/
+
+# Copy to publish path
+if ! [[ -d $publishPath ]]; then
+	mkdir $publishPath
+fi
+	
+cp -r bin/. $publishPath
