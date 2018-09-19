8665051a28eb6d1ac594560f0d46ebc6f8d41496
SvarturH <shalldursson@gmail.com>
2018-09-19 19:34:47 +0000

    Removed unnecessary echo from build script.

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 #Reminders
 read -p "Have you created your php config? [Y/n]:" configCreated
 configCreated=${configCreated:-Y}
-echo $configCreated
 if ! [[ $configCreated == "y" || $configCreated == "Y" ]]; then
 	echo "Please create your php config file using the template provided."
 	exit 1
@@ -25,5 +24,5 @@ cp -r .htaccess builder css items maintenance.html php wiki error index.html js 
 if ! [[ -d $publishPath ]]; then
 	mkdir $publishPath
 fi
-	
+
 cp -r bin/. $publishPath
