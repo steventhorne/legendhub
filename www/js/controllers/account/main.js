8e399adf2296d4117a1e3abe0d1fbbb66c872b3d
SvarturH <shalldursson@gmail.com>
2018-11-19 23:12:44 +0000

    Improved javascript on account page.

--- www/js/controllers/account/main.js
+++ www/js/controllers/account/main.js
@@ -1,41 +1,52 @@
-app.controller('account', ['$scope', '$http', function($scope, $http) {
+app.controller('account', ['$scope', '$http', '$q', function($scope, $http, $q) {
     /** Initializes the controller */
 	$scope.init = function() {
         $scope.notificationOptions = [{Value: 0, Name: 'Off'},
                                       {Value: 1, Name: 'On'}];
-        $scope.getNotificationSettingsAsync();
+        $scope.getNotificationSettingsAsync().then(
+            function (data) {
+                $scope.notificationSettings = data;
+            });
 	}
 
-    /** Gets the notification settings for the current user.
+    /**
+     * Gets the notification settings for the current user.
      *
-     * Gets the settings from the db and fills them into the
-     * $scope.notificationSettings variable.
+     * @return {Promise} Promise object with notification settings.
      */
     $scope.getNotificationSettingsAsync = function() {
+        var deferred = $q.defer();
+
         $http({
 			url: '/php/account/getNotificationSettings.php'
 		}).then(function succcessCallback(response) {
-            $scope.notificationSettings = response.data;
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
+
+        return deferred.promise;
     }
 
-    /** Updates the notification settings for the current user in the DB
+    /**
+     * Updates the notification settings for the current user in the DB
      * using the $scope.notificationSettings variable.
+     *
+     * @param {Array} settings - notificationSettings array that needs to be saved.
      */
-    $scope.saveNotifications = function() {
+    $scope.saveNotifications = function(settings) {
         $http({
             url: '/php/account/updateNotificationSettings.php',
             method: 'POST',
-            data: $scope.notificationSettings
+            data: settings
         }).then(function successCallback(response) {
             $scope.editingNotifications = false;
         }, function errorCallback(response) {
         });
     }
 
-    /** Cancels the editing of the notification settings.
+    /**
+     * Cancels the editing of the notification settings.
      *
      * Replaces the current dirty settings with the settings from the DB.
      */
@@ -44,7 +55,8 @@ app.controller('account', ['$scope', '$http', function($scope, $http) {
         $scope.editingNotifications = false;
     }
 
-    /** Checks the new passwords to see if they match and sets the validity
+    /**
+     * Checks the new passwords to see if they match and sets the validity
      * on the form if they do not.
      */
     $scope.onPasswordChanged = function() {
@@ -52,14 +64,19 @@ app.controller('account', ['$scope', '$http', function($scope, $http) {
             $scope.newPassword === $scope.confirmPassword);
     }
 
-    /** Updates the user's password to the $scope's new password. */
-    $scope.savePassword = function() {
+    /**
+     * Updates the user's password to the $scope's new password.
+     *
+     * @param {string} oldPassword - The old password
+     * @param {string} newPassword - The new Password
+     */
+    $scope.savePassword = function(oldPassword, newPassword) {
         $scope.dbfail = false;
         $http({
             url: '/php/account/updatePassword.php',
             method: 'POST',
-            data: {'oldPassword': $scope.oldPassword,
-                    'newPassword': $scope.newPassword}
+            data: {'oldPassword': oldPassword,
+                    'newPassword': newPassword}
         }).then(function successCallback(response) {
             $scope.dbfail = !response.data.success;
             if (response.success) {
