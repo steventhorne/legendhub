f209e8a1135d1ec72b382293a49c21321ef04364
SvarturH <shalldursson@gmail.com>
2018-12-05 00:08:59 +0000

    Cleaned up JavaScript on Mob pages.

--- www/js/controllers/mobs/edit.js
+++ www/js/controllers/mobs/edit.js
@@ -1,48 +1,92 @@
 angular.module("legendwiki-app").requires.push('ng-showdown');
 
-app.controller('mobs-edit', function($scope, $http, breadcrumb) {
-	$scope.initialize = function() {
-		$scope.mobModel = {"Xp": 0, "Gold": 0, "Aggro": false, "Notes": null};
-		$scope.areas = [];
+app.controller('mobs-edit', ['$scope', '$http', '$q', 'breadcrumb', function($scope, $http, $q, breadcrumb) {
+    /** Initializes the controller. */
+	var initialize = function() {
+        loadPage();
+	};
 
-		$scope.getAreas();
-	}
+    /** Runs the async functions needed before displaying the page. */
+    var loadPage = function() {
+        $q.all([getAreasAsync(), getMobAsync(getUrlParameter("id"))]).then(
+            function(data) {
+                // getAreasAsync
+			    $scope.areas = data[0];
 
-	$scope.getAreas = function() {
+                // getMobAsync
+                $scope.mobModel = data[1];
+
+                if (data[1] == null) {
+                    breadcrumb.links = [{'display': 'Mobs', 'href': '/mobs/'},
+                                        {'display': 'Not Found', 'href': '', 'active': true}];
+                    return;
+                }
+
+                if ($scope.mobModel) {
+                    $scope.mobModel.Aggro = Boolean($scope.mobModel.Aggro);
+                    $scope.initialMobModel = angular.copy($scope.mobModel);
+
+                    breadcrumb.links = [{'display': 'Mobs', 'href': '/mobs/'},
+                                        {'display': $scope.mobModel.AreaEra, 'href': '/mobs/index.html?eraId=' + $scope.mobModel.EraId},
+                                        {'display': $scope.mobModel.AreaName, 'href': '/mobs/index.html?eraId=' + $scope.mobModel.EraId + '&areaId=' + $scope.mobModel.AreaId},
+                                        {'display': $scope.mobModel.Name, 'href': '/mobs/details.html?id=' + $scope.mobModel.Id},
+                                        {'display': 'Edit', 'href': '', 'active': true}];
+                }
+            }
+        );
+    };
+
+    /**
+     * Gets the area data from the server.
+     *
+     * @return {Promise} a promise containing the area data.
+     */
+	var getAreasAsync = function() {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/mobs/getAreas.php'
 		}).then(function successCallback(response) {
-			$scope.areas = response.data;
-			$scope.getMob();
+            deferred.resolve(response.data);
 		}, function errorCallback(response) {
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.getMob = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Gets the mob data from the server.
+     *
+     * @param {int} id - the id of the mob to retrieve.
+     * @return {Promise} a promise containing the mob data.
+     */
+	var getMobAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/mobs/getMob.php',
 			method: 'POST',
-			data: {"id": getUrlParameter("id")}
+			data: {"id": id}
 		}).then(function succcessCallback(response) {
-			$scope.mobModel = response.data;
-			$scope.mobModel.Aggro = Boolean($scope.mobModel.Aggro);
-			$scope.initialMobModel = angular.copy($scope.mobModel);
-
-			breadcrumb.links = [{'display': 'Mobs', 'href': '/mobs/'},
-								{'display': $scope.mobModel.AreaEra, 'href': '/mobs/index.html?eraId=' + $scope.mobModel.EraId},
-								{'display': $scope.mobModel.AreaName, 'href': '/mobs/index.html?eraId=' + $scope.mobModel.EraId + '&areaId=' + $scope.mobModel.AreaId},
-								{'display': $scope.mobModel.Name, 'href': '/mobs/details.html?id=' + $scope.mobModel.Id},
-								{'display': 'Edit', 'href': '', 'active': true}];
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.saveDisabled = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Get whether the save button should be disabled.
+     *
+     * @return {bool} true if the save button should be disabled.
+     */
+	$scope.shouldDisableSave = function() {
 		return !$scope.form.$valid || angular.toJson($scope.mobModel) === angular.toJson($scope.initialMobModel);
-	}
+	};
 
+    /** Submit the mob data to the server. */
 	$scope.submitMob = function() {
 		if (!$scope.form.$valid || angular.toJson($scope.mobModel) === angular.toJson($scope.initialMobModel)) {
 			return;
@@ -58,7 +102,7 @@ app.controller('mobs-edit', function($scope, $http, breadcrumb) {
 		}, function errorCallback(response){
 
 		});
-	}
+	};
 
-	$scope.initialize();
-});
+	initialize();
+}]);
--- www/js/controllers/mobs/main.js
+++ www/js/controllers/mobs/main.js
@@ -1,5 +1,6 @@
-app.controller('mobs', function($scope, $http, categories) {
-	$scope.init = function() {
+app.controller('mobs', ['$scope', '$http', '$q', 'categories', function($scope, $http, $q, categories) {
+    /** Initializes the controller. */
+	var initialize = function() {
 		$scope.mobsPerPage = 20;
 		$scope.sortProperty = "";
 		$scope.sortReverse = false;
@@ -10,74 +11,130 @@ app.controller('mobs', function($scope, $http, categories) {
 		categories.setSelectedSubcategory(getUrlParameter('areaId'));
 		$scope.searchString = getUrlParameter('search');
 
-		$scope.getAreas();
+        loadPage();
+	};
 
-		if (categories.hasSelectedCategory() || $scope.searchString) {
-			$scope.search();
-		}
-		else {
-			$scope.getRecentMobs();
-		}
-	}
+    /** Calls the async functions needed before displaying the page. */
+    var loadPage = function() {
+        getAreasAsync().then(
+            function(data) {
+                var eras = [];
+                var eraCount = 0;
+                for (var i = 0; i < data.length; ++i) {
+                    if (!eras.includes(data[i].Era)) {
+                        eras[data[i].EraId] = data[i].Era;
+                    }
+                    data[i].CategoryId = data[i].EraId;
+                }
 
-	$scope.getAreas = function() {
+                var eraCategories = [];
+                for (var i = 1; i < eras.length; ++i) {
+                    eraCategories.push({Id: i, Name: eras[i]});
+                }
+
+                categories.setCategories(eraCategories);
+                categories.setSubcategories(data);
+
+                if (categories.hasSelectedCategory() || $scope.searchString) {
+                    searchAsync($scope.searchString, categories.getCategoryId(), categories.getSubcategoryId()).then(
+                        function(data) {
+                            $scope.mobs = data;
+                            $scope.recent = false;
+
+                            $scope.totalPages = Math.floor(($scope.mobs.length - 1) / $scope.mobsPerPage) + 1;
+                            $scope.currentPage = 1;
+                        }
+                    );
+                }
+                else {
+                    getRecentMobsAsync().then(
+                        function(data) {
+                            $scope.mobs = data;
+                            $scope.recent = true;
+
+                            $scope.totalPages = Math.floor(($scope.mobs.length - 1) / $scope.mobsPerPage) + 1;
+                            $scope.currentPage = 1;
+                        }
+                    );
+                }
+            }
+        );
+    };
+
+    /**
+     * Gets the area data from the server.
+     *
+     * @return {Promise} a promise containing the area data.
+     */
+	var getAreasAsync = function() {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/mobs/getAreas.php'
 		}).then(function succcessCallback(response) {
-			var eras = [];
-			var eraCount = 0;
-			for (var i = 0; i < response.data.length; ++i) {
-				if (!eras.includes(response.data[i].Era)) {
-					eras[response.data[i].EraId] = response.data[i].Era;
-				}
-				response.data[i].CategoryId = response.data[i].EraId;
-			}
+            deferred.resolve(response.data);
+        }, function errorCallback(response) {
+            deferred.reject(response);
+        });
 
-			var eraCategories = [];
-			for (var i = 1; i < eras.length; ++i) {
-				eraCategories.push({Id: i, Name: eras[i]});
-			}
+        return deferred.promise;
+	};
 
-			categories.setCategories(eraCategories);
-			categories.setSubcategories(response.data);
-		})
-	}
+    /**
+     * Gets the recently added/edited mobs from the server.
+     *
+     * @return {Promise} a promise containing the recent mob data.
+     */
+	var getRecentMobsAsync = function() {
+        var deferred = $q.defer();
 
-	$scope.getRecentMobs = function() {
 		$http({
 			url: '/php/mobs/getRecentMobs.php',
 			method: 'POST'
 		}).then(function succcessCallback(response) {
-			$scope.mobs = response.data;
-			$scope.recent = true;
-
-			$scope.totalPages = Math.floor(($scope.mobs.length - 1) / $scope.mobsPerPage) + 1;
-			$scope.currentPage = 1;
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.search = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Gets the mobs based on search/filter parameters
+     *
+     * @param {string} searchString - the string used to filter out which mobs are returned.
+     * @param {int} eraId - the era that the mob must be a part of.
+     * @param {int} areaId - the area the the mob must be a part of.
+     */
+	var searchAsync = function(searchString, eraId, areaId) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/mobs/getMobs.php',
 			method: 'POST',
-			data: {"searchString": $scope.searchString, "eraId": categories.getCategoryId(), "areaId": categories.getSubcategoryId()}
+			data: {"searchString": searchString, "eraId": eraId, "areaId": areaId}
 		}).then(function succcessCallback(response) {
-			$scope.mobs = response.data;
-			$scope.recent = false;
-
-			$scope.totalPages = Math.floor(($scope.mobs.length - 1) / $scope.mobsPerPage) + 1;
-			$scope.currentPage = 1;
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
+        return deferred.promise;
+	};
+
+    /** Event for when the search button is clicked. */
 	$scope.onSearchClicked = function() {
 		window.location = $scope.getSearchUrl();
-	}
+	};
 
+    /**
+     * Gets the search url based on the category and subcategory ids.
+     *
+     * @param {int} categoryId - the category to search on.
+     * @param {int} subcategoryId - the subcategory to search on.
+     * @return {string} the search url to visit.
+     */
 	$scope.getSearchUrl = function(categoryId, subcategoryId) {
 		var url = "/mobs/index.html?";
 
@@ -97,30 +154,47 @@ app.controller('mobs', function($scope, $http, categories) {
 
 		url += "search=" + $scope.searchString;
 		return url;
-	}
+	};
 
+    /** Event for when the previous button is clicked in the pagination. */
 	$scope.onPreviousClicked = function() {
 		$scope.currentPage -= 1;
 		if ($scope.currentPage < 1) {
 			$scope.currentPage = 1;
 		}
-	}
+	};
 
+    /** Event for when the next button is clicked in the pagination. */
 	$scope.onNextClicked = function() {
 		$scope.currentPage += 1;
 		if ($scope.currentPage > $scope.totalPages) {
 			$scope.currentPage = $scope.totalPages;
 		}
-	}
+	};
 
+    /**
+     * Event for when a specific page is clicked in the pagination.
+     *
+     * @param {int} num - the number of the page that was clicked.
+     */
 	$scope.onPageClicked = function(num) {
 		$scope.currentPage = num;
-	}
+	};
 
+    /**
+     * Event for when a mob is clicked in the search view.
+     *
+     * @param {object} item - the mob that was clicked.
+     */
 	$scope.onMobClicked = function(item) {
 		window.location = "/mobs/details.html?id=" + item.Id;
-	}
+	};
 
+    /**
+     * Gets an array with the page numbers that should be displayed.
+     *
+     * @return {Array} an array of page numbers.
+     */
 	$scope.getPageArray = function() {
 		var nums = [];
 		if ($scope.totalPages > 7) {
@@ -144,8 +218,13 @@ app.controller('mobs', function($scope, $http, categories) {
 			}
 		}
 		return nums;
-	}
+	};
 
+    /**
+     * Event for when a column header is clicked on the search result table.
+     *
+     * @param {string} the variable name of the stat that was clicked.
+     */
 	$scope.onColumnHeaderClicked = function(statVar) {
 		if ($scope.sortProperty == statVar) {
 			$scope.sortReverse = !$scope.sortReverse;
@@ -154,8 +233,14 @@ app.controller('mobs', function($scope, $http, categories) {
 			$scope.sortProperty = statVar;
 			$scope.sortReverse = true;
 		}
-	}
+	};
 
+    /**
+     * Gets the HTML class for the specified stat.
+     *
+     * @param {string} statVar - the variable name of the stat to get the class for.
+     * @return {string} the HTML class names that should be used.
+     */
 	$scope.sortClass = function(statVar) {
 		if (!$scope.sortProperty) {
 			return "fas fa-sort";
@@ -163,7 +248,7 @@ app.controller('mobs', function($scope, $http, categories) {
 		else if ($scope.sortProperty == statVar) {
 			return $scope.sortReverse ? "fas fa-sort-down" : "fas fa-sort-up";
 		}
-	}
+	};
 
-	$scope.init();
-});
+	initialize();
+}]);
