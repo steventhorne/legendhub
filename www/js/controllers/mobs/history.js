bb9da38bff86cb2cf6fe6d5ab4dc67d8e7d2b89b
SvarturH <shalldursson@gmail.com>
2018-12-05 00:08:59 +0000

    Cleaned up JavaScript on Mob pages.

--- www/js/controllers/mobs/history.js
+++ www/js/controllers/mobs/history.js
@@ -1,61 +1,120 @@
 angular.module("legendwiki-app").requires.push('ng-showdown');
 
-app.controller('mobs-history', function($scope, $http, itemConstants, breadcrumb) {
-	$scope.getMob = function() {
+app.controller('mobs-history', ['$scope', '$http', '$q', 'itemConstants', 'breadcrumb', function($scope, $http, $q, itemConstants, breadcrumb) {
+    /** Initializes the controller. */
+    var initialize = function() {
 		$scope.slots = itemConstants.slots;
+
+        loadPage();
+    };
+
+    /** Runs the async functions needed before displaying the page. */
+    var loadPage = function() {
+        getMobAsync(getUrlParameter("id")).then(
+            function(data) {
+                // getMobAsync
+                $scope.mob = data;
+                if (data == null) {
+                    breadcrumb.links = [{'display': 'Mobs', 'href': '/mobs/'},
+                                        {'display': 'Not Found', 'href': '', 'active': true}];
+                    return;
+                }
+
+                $scope.mob.ModifiedOn = (new Date($scope.mob.ModifiedOn + " UTC")).toString().slice(4, 24);
+                $scope.mob.Id = $scope.mob.MobId;
+
+                breadcrumb.links = [{'display': 'Mobs', 'href': '/mobs/'},
+                                    {'display': $scope.mob.AreaEra, 'href': '/mobs/index.html?eraId=' + $scope.mob.EraId},
+                                    {'display': $scope.mob.AreaName, 'href': '/mobs/index.html?eraId=' + $scope.mob.EraId + '&areaId=' + $scope.mob.AreaId},
+                                    {'display': $scope.mob.Name, 'href': '/mobs/details.html?id=' + $scope.mob.Id},
+                                    {'display': $scope.mob.ModifiedOn, 'href': '', 'active': true}];
+
+                $q.all([getMobHistoryAsync($scope.mob.MobId), getItemsAsync($scope.mob.MobId)]).then(
+                    function(data) {
+                        // getMobHistoryAsync
+                        $scope.history = data[0].slice(0, 9);
+                        for (let i = 0; i < $scope.history.length; i++) {
+                            $scope.history[i].ModifiedOn = (new Date($scope.history[i].ModifiedOn + " UTC")).toString().slice(4, 24);
+                        }
+
+                        // getItemsAsync
+                        $scope.items = data[1];
+                        for (let i = 0; i < $scope.items.length; ++i) {
+                            $scope.items[i].SlotName = $scope.slots[$scope.items[i].Slot];
+                        }
+                    }
+                );
+            }
+        );
+    };
+
+    /**
+     * Gets the mob data from the server.
+     *
+     * @param {int} id - the id of the mob history to retrieve.
+     * @return {Promise} a promise containing the mob data.
+     */
+	var getMobAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/mobs/getMobByHistoryId.php',
 			method: 'POST',
-			data: {"id": getUrlParameter("id")}
+			data: {"id": id}
 		}).then(function succcessCallback(response) {
-			$scope.mob = response.data;
-			$scope.mob.ModifiedOn = (new Date(response.data.ModifiedOn + " UTC")).toString().slice(4, 24);
-			$scope.mob.Id = $scope.mob.MobId;
-
-			breadcrumb.links = [{'display': 'Mobs', 'href': '/mobs/'},
-								{'display': $scope.mob.AreaEra, 'href': '/mobs/index.html?eraId=' + $scope.mob.EraId},
-								{'display': $scope.mob.AreaName, 'href': '/mobs/index.html?eraId=' + $scope.mob.EraId + '&areaId=' + $scope.mob.AreaId},
-								{'display': $scope.mob.Name, 'href': '/mobs/details.html?id=' + $scope.mob.Id},
-								{'display': $scope.mob.ModifiedOn, 'href': '', 'active': true}];
-
-			$scope.getMobHistory();
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.getMobHistory = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Gets the mob history data from the server.
+     *
+     * @param {int} id - the id of the mob to retrieve.
+     * @return {Promise} a promise containing the mob history data.
+     */
+	var getMobHistoryAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/mobs/getMobHistory.php',
 			method: 'POST',
-			data: {"id": $scope.mob.MobId}
+			data: {"id": id}
 		}).then(function succcessCallback(response) {
-			$scope.history = response.data.slice(0, 9);
-			var i;
-			for (i = 0; i < $scope.history.length; i++) {
-				$scope.history[i].ModifiedOn = (new Date($scope.history[i].ModifiedOn + " UTC")).toString().slice(4, 24);
-			}
-			$scope.getItems();
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.getItems = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Gets the items linked to this mob.
+     *
+     * @param {int} id - the id of the mob to get the linked items for.
+     * @return {Promise} a promise containing the item data.
+     */
+    var getItemsAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/items/getItemsByMobId.php',
 			method: 'POST',
-			data: {"MobId": $scope.mob.MobId}
+			data: {"MobId": id}
 		}).then(function successCallback(response) {
-			$scope.items = response.data;
-			for (var i = 0; i < $scope.items.length; ++i) {
-				$scope.items[i].SlotName = $scope.slots[$scope.items[i].Slot];
-			}
+            deferred.resolve(response.data);
 		}, function errorCallback(response) {
-
+            deferred.reject(response.data);
 		});
-	}
 
+        return deferred.promise;
+	};
+
+    /** Reverts the mob to this history version of the mob. */
 	$scope.revert = function() {
 		var postData = angular.copy($scope.mob);
 		delete postData.MobId;
@@ -70,20 +129,8 @@ app.controller('mobs-history', function($scope, $http, itemConstants, breadcrumb
 		}).then(function succcessCallback(response) {
 			window.location = "/mobs/details.html?id=" + $scope.mob.MobId;
 		}, function errorCallback(response){
-
 		});
-	}
+	};
 
-	$scope.initialize = function() {
-		$http({
-			url: '/php/login/getLoggedInUser.php'
-		}).then(function succcessCallback(response) {
-			$scope.isLoggedIn = response.data.success;
-			$scope.getMob();
-		}, function errorCallback(response) {
-
-		});
-	}
-
-	$scope.initialize();
-});
+	initialize();
+}]);
