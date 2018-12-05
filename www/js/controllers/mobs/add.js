b9ecd48ae61dd63c4ae0e0061bd0a1c75de1a95c
SvarturH <shalldursson@gmail.com>
2018-12-05 00:08:59 +0000

    Cleaned up JavaScript on Mob pages.

--- www/js/controllers/mobs/add.js
+++ www/js/controllers/mobs/add.js
@@ -1,37 +1,60 @@
 angular.module("legendwiki-app").requires.push('ng-showdown');
 
-app.controller('mobs-add', function($scope, $http, breadcrumb) {
-	$scope.initialize = function() {
+app.controller('mobs-add', ['$scope', '$http', '$q', 'breadcrumb', function($scope, $http, $q, breadcrumb) {
+    /** Initializes the controller. */
+	var initialize = function() {
 		$scope.mobModel = {"Xp": 0, "Gold": 0, "Aggro": false, "Notes": null};
 		$scope.areas = [];
 
 		breadcrumb.links = [{'display': 'Mobs', 'href': '/mobs/'},
 						{'display': 'Add', 'href': '', 'active': true}];
 
-		$scope.getAreas();
-	}
+        loadPage();
+	};
 
-	$scope.getAreas = function() {
+    /** Calls the async functions that are needed before displaying the page. */
+    var loadPage = function() {
+        getAreasAsync().then(
+            function(data) {
+                $scope.areas = data;
+            }
+        );
+    };
+
+    /**
+     * Gets the areas from the server.
+     *
+     * @return {Promise} a promise containing the area data.
+     */
+	var getAreasAsync = function() {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/mobs/getAreas.php',
 		}).then(function successCallback(response) {
-			$scope.areas = response.data;
+            deferred.resolve(response.data);
 		}, function errorCallback(response) {
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.submitMob = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Submits the mob data to the server.
+     *
+     * @param {object} mob - the mob to be submitted.
+     */
+	$scope.submitMob = function(mob) {
 		$http({
 			url: '/php/mobs/insertMob.php',
 			method: 'POST',
-			data: $scope.mobModel
+			data: mob
 		}).then(function succcessCallback(response) {
 			window.location = "/mobs/details.html?id=" + response.data;
 		}, function errorCallback(response){
-
 		});
-	}
+	};
 
-	$scope.initialize();
-});
+	initialize();
+}]);
--- www/js/controllers/mobs/details.js
+++ www/js/controllers/mobs/details.js
@@ -1,70 +1,115 @@
 angular.module("legendwiki-app").requires.push('ng-showdown');
 
-app.controller('mobs-details', function($scope, $http, itemConstants, breadcrumb) {
-	$scope.initialize = function() {
+app.controller('mobs-details', ['$scope', '$http', '$q', 'itemConstants', 'breadcrumb', function($scope, $http, $q, itemConstants, breadcrumb) {
+    /** Initializes the controller. */
+	var initialize = function() {
 		$scope.slots = itemConstants.slots;
 
-		$http({
-			url: '/php/login/getLoggedInUser.php'
-		}).then(function succcessCallback(response) {
-			$scope.isLoggedIn = response.data.success;
-			$scope.getMob();
-		}, function errorCallback(response) {
+        loadPage();
+	};
 
-		});
-	}
+    /** Calls the async functions needed before displaying the page. */
+    var loadPage = function() {
+        var id = getUrlParameter("id");
+        $q.all([getMobAsync(id), getMobHistoryAsync(id), getItemsAsync(id)]).then(
+            function(data) {
+                // getMobAsync
+                $scope.mob = data[0];
+                if (data[0] == null) {
+                    breadcrumb.links = [{'display': 'Mobs', 'href': '/mobs/'},
+                                        {'display': 'Not Found', 'href': '', 'active': true}];
+                    return;
+                }
 
-	$scope.getMob = function() {
+                if ($scope.mob) {
+                    $scope.mob.ModifiedOn = (new Date($scope.mob.ModifiedOn + " UTC")).toString().slice(4, 24);
+
+                    breadcrumb.links = [{'display': 'Mobs', 'href': '/mobs/'},
+                                        {'display': $scope.mob.AreaEra, 'href': '/mobs/index.html?eraId=' + $scope.mob.EraId},
+                                        {'display': $scope.mob.AreaName, 'href': '/mobs/index.html?eraId=' + $scope.mob.EraId + '&areaId=' + $scope.mob.AreaId},
+                                        {'display': $scope.mob.Name, 'href': '', 'active': true}];
+                }
+
+                // getMobHistoryAsync
+                $scope.history = data[1].slice(0, 9);
+                for (let i = 0; i < $scope.history.length; i++) {
+                    $scope.history[i].ModifiedOn = (new Date($scope.history[i].ModifiedOn + " UTC")).toString().slice(4, 24);
+                }
+
+                //getItemsAsync
+                $scope.items = data[2];
+                for (var i = 0; i < $scope.items.length; ++i) {
+                    $scope.items[i].SlotName = $scope.slots[$scope.items[i].Slot];
+                }
+            }
+        )
+    };
+
+    /**
+     * Gets the mob data from the server.
+     *
+     * @param {int} id - the id of the mob to retrieve.
+     * @return {Promise} a promise containing the mob data.
+     */
+	var getMobAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/mobs/getMob.php',
 			method: 'POST',
-			data: {"id": getUrlParameter("id")}
+			data: {"id": id}
 		}).then(function succcessCallback(response) {
-			$scope.mob = response.data;
-			$scope.mob.ModifiedOn = (new Date(response.data.ModifiedOn + " UTC")).toString().slice(4, 24);
-
-			breadcrumb.links = [{'display': 'Mobs', 'href': '/mobs/'},
-								{'display': $scope.mob.AreaEra, 'href': '/mobs/index.html?eraId=' + $scope.mob.EraId},
-								{'display': $scope.mob.AreaName, 'href': '/mobs/index.html?eraId=' + $scope.mob.EraId + '&areaId=' + $scope.mob.AreaId},
-								{'display': $scope.mob.Name, 'href': '', 'active': true}];
-
-			$scope.getMobHistory();
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.getMobHistory = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Gets the mob history data from the server.
+     *
+     * @param {int} id - the id of the mob to retrieve history data for.
+     * @return {Promise} a promise containing the mob history data.
+     */
+	var getMobHistoryAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/mobs/getMobHistory.php',
 			method: 'POST',
-			data: {"id": getUrlParameter("id")}
+			data: {"id": id}
 		}).then(function succcessCallback(response) {
-			$scope.history = response.data;
-			var i;
-			for (i = 0; i < $scope.history.length; i++) {
-				$scope.history[i].ModifiedOn = (new Date($scope.history[i].ModifiedOn + " UTC")).toString().slice(4, 24);
-			}
-			$scope.getItems();
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.getItems = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Gets the linked items for the specified mob.
+     *
+     * @param {int} id - the id of the mob to retrieve linked items for.
+     * @return {Promise} a promise containing the item data.
+     */
+	var getItemsAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/items/getItemsByMobId.php',
 			method: 'POST',
-			data: {"MobId": getUrlParameter("id")}
+			data: {"MobId": id}
 		}).then(function successCallback(response) {
-			$scope.items = response.data;
-			for (var i = 0; i < $scope.items.length; ++i) {
-				$scope.items[i].SlotName = $scope.slots[$scope.items[i].Slot];
-			}
+            deferred.resolve(response.data);
 		}, function errorCallback(response) {
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.initialize();
-});
+        return deferred.promise;
+	};
+
+	initialize();
+}]);
