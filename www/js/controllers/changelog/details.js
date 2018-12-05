e6951a3703b7387dfbea43d5d17c12738cbe7c5b
SvarturH <shalldursson@gmail.com>
2018-12-05 00:07:30 +0000

    Cleaned up JavaScript on changelog pages.

--- www/js/controllers/changelog/details.js
+++ www/js/controllers/changelog/details.js
@@ -1,30 +1,47 @@
 angular.module("legendwiki-app").requires.push('ng-showdown');
 
-app.controller('changelog-details', ['$scope', '$http', 'breadcrumb', function($scope, $http, breadcrumb) {
+app.controller('changelog-details', ['$scope', '$http', '$q', 'breadcrumb', function($scope, $http, $q, breadcrumb) {
+    /** Initializes the controller. */
 	$scope.initialize = function() {
-		$scope.getChangelogVersion();
-    }
+        $scope.getChangelogVersionAsync(getUrlParameter("id")).then(
+            function(data) {
+                $scope.changelogModel = data;
 
-	$scope.getChangelogVersion = function() {
+                $scope.changelogModel.ModifiedOn = (new Date(data.ModifiedOn + " UTC")).toString().slice(4, 24);
+                $scope.changelogModel.CreatedOn = (new Date(data.CreatedOn + " UTC")).toString().slice(4, 15);
+
+                $scope.addBreadcrumb();
+            }
+        );
+    };
+
+    /**
+     * Gets the changelog version with the specified Id.
+     *
+     * @param {int} id - The Id of the changelog version that is needed.
+     * @return {Promise} a promise containing the changelog version requested.
+     */
+	$scope.getChangelogVersionAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/changelog/getChangelogVersion.php',
 			method: 'POST',
-			data: {"id": getUrlParameter("id")}
+			data: {"id": id}
 		}).then(function succcessCallback(response) {
-			$scope.changelogModel = response.data;
-			$scope.changelogModel.ModifiedOn = (new Date(response.data.ModifiedOn + " UTC")).toString().slice(4, 24);
-			$scope.changelogModel.CreatedOn = (new Date(response.data.CreatedOn + " UTC")).toString().slice(4, 15);
-
-            $scope.addBreadcrumb();
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
+        return deferred.promise;
+	};
+
+    /** Adds the breadcrumb links */
     $scope.addBreadcrumb = function() {
         breadcrumb.links = [{'display': 'Changelog', 'href': '/changelog/'},
 								{'display': $scope.changelogModel.Version, 'href': '', 'active': true}];
-    }
+    };
 
 	$scope.initialize();
 }]);
--- www/js/controllers/changelog/main.js
+++ www/js/controllers/changelog/main.js
@@ -1,21 +1,35 @@
-app.controller('changelog', function($scope, $cookies, $http, itemConstants, categories) {
+app.controller('changelog', ['$scope', '$cookies', '$http', '$q', function($scope, $cookies, $http, $q) {
+    /** Initializes the controller. */
 	$scope.init = function() {
-        $scope.getChangelogVersions();
-	}
+        $scope.getChangelogVersionsAsync().then(
+            function(data) {
+                $scope.versions = data;
 
-    $scope.getChangelogVersions = function() {
+                for (var i = 0; i < $scope.versions.length; ++i) {
+			        $scope.versions[i].CreatedOn = (new Date($scope.versions[i].CreatedOn + " UTC")).toString().slice(4, 15);
+                }
+            }
+        );
+	};
+
+    /**
+     * Gets the list of changelog versions.
+     *
+     * @return {Promise} promise containing the changelog versions array.
+     */
+    $scope.getChangelogVersionsAsync = function() {
+        var deferred = $q.defer();
+
         $http({
 			url: '/php/changelog/getChangelogVersions.php'
 		}).then(function succcessCallback(response) {
-			$scope.versions = response.data;
-
-            for (var i = 0; i < $scope.versions.length; ++i) {
-			    $scope.versions[i].CreatedOn = (new Date($scope.versions[i].CreatedOn + " UTC")).toString().slice(4, 15);
-            }
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-    }
+
+        return deferred.promise;
+    };
 
 	$scope.init();
-});
+}]);
