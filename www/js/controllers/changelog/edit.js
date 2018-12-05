aee33298edfe7f92373c889ba398232bbbfa40b8
SvarturH <shalldursson@gmail.com>
2018-12-05 00:07:30 +0000

    Cleaned up JavaScript on changelog pages.

--- www/js/controllers/changelog/edit.js
+++ www/js/controllers/changelog/edit.js
@@ -1,47 +1,73 @@
 angular.module("legendwiki-app").requires.push('ng-showdown');
 
-app.controller('changelog-edit', function($scope, $http, breadcrumb) {
+app.controller('changelog-edit', ['$scope', '$http', '$q', 'breadcrumb', function($scope, $http, $q, breadcrumb) {
+    /** Initializes the controller. */
 	$scope.initialize = function() {
 		$scope.changelogModel = {Title: "", CategoryId: 0, SubCategoryId: 0, Tags: "", Content: ""};
-		$scope.getChangelogVersion();
-	}
+        $scope.getChangelogVersionAsync(getUrlParameter("id")).then(
+            function(data) {
+                $scope.changelogModel = data;
+                $scope.initialChangelogModel = angular.copy($scope.changelogModel);
 
-	$scope.getChangelogVersion = function() {
+                $scope.addBreadcrumbs();
+            }
+        );
+	};
+
+    /**
+     * Gets the changelog version with the given id.
+     *
+     * @param {int} id - the Id of the changelog requested.
+     * @return {Promise} a promise containing the requested changelog version.
+     */
+	$scope.getChangelogVersionAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/changelog/getChangelogVersion.php',
 			method: 'POST',
-			data: {"id": getUrlParameter("id")}
+			data: {"id": id}
 		}).then(function succcessCallback(response) {
-			$scope.changelogModel = response.data;
-			$scope.initialChangelogModel = angular.copy($scope.changelogModel);
-
-			$scope.addBreadcrumbs();
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
+        return deferred.promise;
+	};
+
+    /** Adds breadcrumb links. */
 	$scope.addBreadcrumbs = function() {
 		breadcrumb.links = [{'display': 'Changelog', 'href': '/changelog/'},
 		                    {'display': $scope.changelogModel.Version, 'href': '/changelog/details.html?id=' + $scope.changelogModel.Id},
 		                    {'display': 'Edit', 'href': '', 'active': true}];
-	}
+	};
 
-	$scope.saveDisabled = function() {
+    /**
+     * Gets whether the save button should be disabled.
+     *
+     * @return {bool} true if save button should be disabled.
+     */
+	$scope.getIsSaveDisabled = function() {
 		return !$scope.form.$valid || angular.toJson($scope.changelogModel) === angular.toJson($scope.initialChangelogModel);
-	}
+	};
 
-	$scope.submitChangelog = function() {
+    /**
+     * Submits the specified changelog version to the server.
+     *
+     * @param {object} changelog - the changelog version to be submitted.
+     */
+	$scope.submitChangelog = function(changelog) {
 		$http({
 			url: '/php/changelog/updateChangelogVersion.php',
 			method: 'POST',
-			data: $scope.changelogModel
+			data: changelog
 		}).then(function succcessCallback(response) {
 			window.location = "/changelog/details.html?id=" + response.data;
 		}, function errorCallback(response){
 
 		});
-	}
+	};
 
 	$scope.initialize();
-});
+}]);
