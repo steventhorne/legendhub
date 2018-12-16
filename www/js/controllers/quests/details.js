3dccf4fbdfdee79b19b6f9d351a53f9b56dbd052
SvarturH <shalldursson@gmail.com>
2018-12-16 02:54:19 +0000

    Improved javascript for quests pages.

--- www/js/controllers/quests/details.js
+++ www/js/controllers/quests/details.js
@@ -1,76 +1,118 @@
 angular.module("legendwiki-app").requires.push('ng-showdown');
 
-app.controller('quests-details', function($scope, $http, itemConstants, breadcrumb) {
-	$scope.initialize = function() {
+app.controller('quests-details', ['$scope', '$http', '$q', 'itemConstants', 'breadcrumb', function($scope, $http, $q, itemConstants, breadcrumb) {
+    /** Initializes the controller. */
+    var initialize = function() {
 		$scope.slots = itemConstants.slots;
 
-		$http({
-			url: '/php/login/getLoggedInUser.php'
-		}).then(function succcessCallback(response) {
-			$scope.isLoggedIn = response.data.success;
-			$scope.getQuest();
-		}, function errorCallback(response) {
+        loadPage();
+	};
 
-		});
-	}
+    /** Calls the async functions needed to display the page. */
+    var loadPage = function() {
+        var id = getUrlParameter("id");
+        $q.all([getQuestAsync(id), getQuestHistoryAsync(id), getItemsAsync(id)]).then(
+            function(data) {
+                // getQuestAsync
+                $scope.quest = data[0];
 
-	$scope.getQuest = function() {
+                if (data[0] == null) {
+                    breadcrumb.links = [{'display': 'Quests', 'href': '/quests/'},
+                                        {'display': 'Not Found', 'href': '', 'active': true}];
+
+                    return;
+                }
+
+                $scope.quest.ModifiedOn = (new Date($scope.quest.ModifiedOn + " UTC")).toString().slice(4, 24);
+                $scope.quest.Stat = Boolean($scope.quest.Stat);
+
+                breadcrumb.links = [{'display': 'Quests', 'href': '/quests/'},
+                                    {'display': $scope.quest.AreaEra, 'href': '/quests/index.html?eraId=' + $scope.quest.EraId},
+                                    {'display': $scope.quest.AreaName, 'href': '/quests/index.html?eraId=' + $scope.quest.EraId + '&areaId=' + $scope.quest.AreaId},
+                                    {'display': $scope.quest.Title, 'href': '', 'active': true}];
+
+                $scope.whoises = $scope.quest.Whoises.split(';');
+
+                // getQuestHistoryAsync
+                $scope.history = data[1].slice(0, 9);
+                for (let i = 0; i < $scope.history.length; i++) {
+                    $scope.history[i].ModifiedOn = (new Date($scope.history[i].ModifiedOn + " UTC")).toString().slice(4, 24);
+                }
+
+                // getItemsAsync
+                $scope.items = data[2];
+                for (var i = 0; i < $scope.items.length; ++i) {
+                    $scope.items[i].SlotName = $scope.slots[$scope.items[i].Slot];
+                }
+            }
+        );
+    };
+
+    /**
+     * Gets the quest data from the server.
+     *
+     * @param {int} id - the id of the quest to retrieve.
+     * @return {Promise} a promise containing the quest data.
+     */
+	var getQuestAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/quests/getQuest.php',
 			method: 'POST',
-			data: {"id": getUrlParameter("id")}
+			data: {"id": id}
 		}).then(function succcessCallback(response) {
-			$scope.quest = response.data;
-			$scope.quest.ModifiedOn = (new Date(response.data.ModifiedOn + " UTC")).toString().slice(4, 24);
-			$scope.quest.Stat = Boolean($scope.quest.Stat);
-
-			breadcrumb.links = [{'display': 'Quests', 'href': '/quests/'},
-								{'display': $scope.quest.AreaEra, 'href': '/quests/index.html?eraId=' + $scope.quest.EraId},
-								{'display': $scope.quest.AreaName, 'href': '/quests/index.html?eraId=' + $scope.quest.EraId + '&areaId=' + $scope.quest.AreaId},
-								{'display': $scope.quest.Title, 'href': '', 'active': true}];
-
-			$scope.getQuestHistory();
-			$scope.splitWhoises();
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.splitWhoises = function() {
-		$scope.whoises = $scope.quest.Whoises.split(';');
-	}
+        return deferred.promise;
+	};
 
-	$scope.getQuestHistory = function() {
+    /**
+     * Gets the quest history from the server.
+     *
+     * @param {int} id - the id of the quest to retrieve the history for.
+     * @return {Promise} a promise containing the array of quest history data.
+     */
+	var getQuestHistoryAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/quests/getQuestHistory.php',
 			method: 'POST',
-			data: {"id": getUrlParameter("id")}
+			data: {"id": id}
 		}).then(function succcessCallback(response) {
-			$scope.history = response.data;
-			var i;
-			for (i = 0; i < $scope.history.length; i++) {
-				$scope.history[i].ModifiedOn = (new Date($scope.history[i].ModifiedOn + " UTC")).toString().slice(4, 24);
-			}
-			$scope.getItems();
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.getItems = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Gets the items for a quest.
+     *
+     * @param {int} id - the id of the quest to retrieve the items for.
+     * @return {Promise} a promise containing the item array data.
+     */
+	var getItemsAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/items/getItemsByQuestId.php',
 			method: 'POST',
-			data: {"QuestId": getUrlParameter("id")}
+			data: {"QuestId": id}
 		}).then(function successCallback(response) {
-			$scope.items = response.data;
-			for (var i = 0; i < $scope.items.length; ++i) {
-				$scope.items[i].SlotName = $scope.slots[$scope.items[i].Slot];
-			}
+            deferred.resolve(response.data);
 		}, function errorCallback(response) {
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.initialize();
-});
+        return deferred.promise;
+	};
+
+	initialize();
+}]);
--- www/js/controllers/quests/edit.js
+++ www/js/controllers/quests/edit.js
@@ -1,54 +1,96 @@
 angular.module("legendwiki-app").requires.push('ng-showdown');
 
-app.controller('quests-edit', function($scope, $http, breadcrumb) {
-	$scope.initialize = function() {
+app.controller('quests-edit', ['$scope', '$http', '$q', 'breadcrumb', function($scope, $http, breadcrumb) {
+    /** Initializes the controller. */
+	var initialize = function() {
 		$scope.questModel = {Content: "", Whoises: null};
 		$scope.areas = [];
 
 		$scope.getAreas();
-	}
+	};
 
-	$scope.getAreas = function() {
+    var loadPage = function() {
+        $q.all([getAreasAsync(), getQuestAsync(getUrlParameter("id"))]).then(
+            function(data) {
+                // getAreasAsync
+			    $scope.areas = data[0];
+
+                // getQuestAsync
+                $scope.questModel = data[1];
+                $scope.questModel.Aggro = Boolean($scope.questModel.Aggro);
+                $scope.initialQuestModel = angular.copy($scope.questModel);
+
+                breadcrumb.links = [{'display': 'Quests', 'href': '/quests/'},
+                                    {'display': $scope.questModel.AreaEra, 'href': '/quests/index.html?eraId=' + $scope.questModel.EraId},
+                                    {'display': $scope.questModel.AreaName, 'href': '/quests/index.html?eraId=' + $scope.questModel.EraId + '&areaId=' + $scope.questModel.AreaId},
+                                    {'display': $scope.questModel.Title, 'href': '/quests/details.html?id=' + $scope.questModel.Id},
+                                    {'display': 'Edit', 'href': '', 'active': true}];
+            }
+        );
+
+    };
+
+    /**
+     * Gets the area data from the server.
+     *
+     * @return {Promise} a promise containing the area data.
+     */
+	var getAreasAsync = function() {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/mobs/getAreas.php'
 		}).then(function successCallback(response) {
-			$scope.areas = response.data;
-			$scope.getQuest();
+            deferred.resolve(response.data);
 		}, function errorCallback(response) {
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.getQuest = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Gets the quest data from the server.
+     *
+     * @param {int} id - the id of the quest to retrieve.
+     * @return {Promise} a promise containing the quest data.
+     */
+	var getQuestAsync = function(id) {
+        var deferred = $q.defer();
+
 		$http({
 			url: '/php/quests/getQuest.php',
 			method: 'POST',
-			data: {"id": getUrlParameter("id")}
+			data: {"id": id}
 		}).then(function succcessCallback(response) {
-			$scope.questModel = response.data;
-			$scope.questModel.Aggro = Boolean($scope.questModel.Aggro);
-			$scope.initialQuestModel = angular.copy($scope.questModel);
-
-			breadcrumb.links = [{'display': 'Quests', 'href': '/quests/'},
-								{'display': $scope.questModel.AreaEra, 'href': '/quests/index.html?eraId=' + $scope.questModel.EraId},
-								{'display': $scope.questModel.AreaName, 'href': '/quests/index.html?eraId=' + $scope.questModel.EraId + '&areaId=' + $scope.questModel.AreaId},
-								{'display': $scope.questModel.Title, 'href': '/quests/details.html?id=' + $scope.questModel.Id},
-								{'display': 'Edit', 'href': '', 'active': true}];
+            deferred.resolve(response.data);
 		}, function errorCallback(response){
-
+            deferred.reject(response);
 		});
-	}
 
-	$scope.saveDisabled = function() {
+        return deferred.promise;
+	};
+
+    /**
+     * Whether the save button should be disabled.
+     *
+     * @return {bool} True if the save button should be disabled.
+     */
+	$scope.shouldDisableSave = function() {
 		return !$scope.form.$valid || angular.toJson($scope.questModel) === angular.toJson($scope.initialQuestModel);
-	}
+	};
 
-	$scope.submitQuest = function() {
-		if (!$scope.form.$valid || angular.toJson($scope.questModel) === angular.toJson($scope.initialQuestModel)) {
+    /**
+     * Updates the quest data on the server.
+     *
+     * @param {object} questModel - the quest data to submit to the server.
+     */
+	$scope.submitQuest = function(questModel) {
+		if (!$scope.form.$valid || angular.toJson(questModel) === angular.toJson($scope.initialQuestModel)) {
 			return;
 		}
 
-		var postData = angular.copy($scope.questModel);
+		var postData = angular.copy(questModel);
 		$http({
 			url: '/php/quests/updateQuest.php',
 			method: 'POST',
@@ -60,5 +102,5 @@ app.controller('quests-edit', function($scope, $http, breadcrumb) {
 		});
 	}
 
-	$scope.initialize();
-});
+	initialize();
+}]);
