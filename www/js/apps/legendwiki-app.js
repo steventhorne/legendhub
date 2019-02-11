d9a597ea09b5f40144d2a1d6b5a2741fb907e0f4
SvarturH <shalldursson@gmail.com>
2019-02-11 17:48:55 -0500

    updates to cookie consent html

--- www/js/apps/legendwiki-app.js
+++ www/js/apps/legendwiki-app.js
@@ -108,10 +108,6 @@ app.run(function($templateCache) {
 
 });
 
-$templateCache.put('cookieConsent.html',
-'This website or its third-party tools use cookies, which are necessary to its functioning and required to achieve the purposes illustrated in the cookie policy. If you want to know more or withdraw your consent to all or some of the cookies, please refer to the cookie policy.' +
-'By closing this banner, scrolling this page, clicking a link or continuing to browse otherwise, you agree to the use of cookies.');
-
 // http interceptor to redirect all 401/403 responses to the 401 page
 app.factory('unauthorizedInterceptor', function($q) {
 	return {
@@ -775,11 +771,26 @@ app.directive('lhFooter', function() {
 	}
 });
 
-app.directive('lhCookieConsent', function() {
+app.directive('lhCookieConsent', function($cookies) {
 	return {
 		restrict: 'E',
-		templateUrl: 'cookieConsent.html'
-	}
+		link: function(scope, element, attrs) {
+			var consent = $cookies.get("cookie-consent");
+			if (!consent) {
+				var msg = 'This website or its third-party tools use cookies, which are necessary to its functioning and required to achieve the purposes illustrated in the cookie policy. If you want to know more or withdraw your consent to all or some of the cookies, please refer to the cookie policy.' +
+				'By closing this banner, scrolling this page, clicking a link or continuing to browse otherwise, you agree to the use of cookies.';
+			
+				var template = "<div class='cookie-consent-banner'><p>" + msg + "</p><button ng-click='consentToCookies()'>Ok</button></div>";
+
+				scope.consentToCookies = function() {
+					$cookies.put("cookie-consent", true);
+					element.html(null);
+				};
+
+				element.html(template);
+			}
+		}
+	};
 });
 
 app.directive('lhTooltip', function() {
