9b933129cd9f498293cb1a1bd300f4eaaa7b6edc
SvarturH <shalldursson@gmail.com>
2019-02-11 18:03:55 -0500

    moved new cookie html to proper src files.

--- www/js/apps/src/9_directives.js
+++ www/js/apps/src/9_directives.js
@@ -23,6 +23,28 @@ app.directive('lhFooter', function() {
 	}
 });
 
+app.directive('lhCookieConsent', function($cookies) {
+	return {
+		restrict: 'E',
+		link: function(scope, element, attrs) {
+			var consent = $cookies.get("cookie-consent");
+			if (!consent) {
+				var msg = 'This website or its third-party tools use cookies, which are necessary to its functioning and required to achieve the purposes illustrated in the cookie policy. If you want to know more or withdraw your consent to all or some of the cookies, please refer to the cookie policy.' +
+				'By closing this banner, scrolling this page, clicking a link or continuing to browse otherwise, you agree to the use of cookies.';
+			
+				var template = "<div class='cookie-consent-banner'><p>" + msg + "</p><button ng-click='consentToCookies()'>Ok</button></div>";
+
+				scope.consentToCookies = function() {
+					$cookies.put("cookie-consent", true);
+					element.html(null);
+				};
+
+				element.html(template);
+			}
+		}
+	};
+});
+
 app.directive('lhTooltip', function() {
 	return {
 		restrict: 'A',
