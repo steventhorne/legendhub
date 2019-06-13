669183d0a3b2da8de4e3dbd1fec0a5617b4af016
SvarturH <shalldursson@gmail.com>
2019-06-13 00:12:39 +0000

    Updated login page for new api.

--- www_new/public/js/controllers/login.js
+++ www_new/public/js/controllers/login.js
@@ -1,82 +1,69 @@
 app.controller('login', ['$scope', '$cookies', '$http', function($scope, $cookies, $http) {
     /** Initializes the controller */
     var initialize = function() {
-	    $scope.registerFailed = false;
-	    $scope.loginFailed = false;
+        $scope.registerErrors = {};
+        $scope.loginErrors = {};
     };
 
-	$scope.checkUsername = function() {
-		$scope.registerForm.usernameInput.$setValidity("length", null);
-		$scope.registerForm.usernameInput.$setValidity("taken", null);
-		if ($scope.registerModel.Username == '') {
-			return;
-		}
-		$http({
-			url: '/php/login/checkUsername.php',
-			method: 'POST',
-			data: $scope.registerModel
-		}).then(function succcessCallback(response) {
-			$scope.registerForm.usernameInput.$setValidity(response.data.reason, response.data.success);
-		}, function errorCallback(response){
-
-		});
-	}
-
 	$scope.checkPassword = function() {
-		$scope.registerForm.passwordInput.$setValidity("length", null);
-		$scope.registerForm.confirmPasswordInput.$setValidity("length", null);
 		$scope.registerForm.confirmPasswordInput.$setValidity("match", null);
 
-		if ($scope.registerModel.Password && $scope.registerModel.Password.length > 0 && $scope.registerModel.Password.length < 8) {
-			$scope.registerForm.passwordInput.$setValidity("length", false);
-		}
-		if ( $scope.registerModel.ConfirmPassword && $scope.registerModel.ConfirmPassword.length > 0 && $scope.registerModel.ConfirmPassword.length < 8) {
-			$scope.registerForm.confirmPasswordInput.$setValidity("length", false);
-		}
-		if ($scope.registerModel.Password && $scope.registerModel.ConfirmPassword && $scope.registerModel.Password.length > 0 && $scope.registerModel.ConfirmPassword.length > 0 && $scope.registerModel.Password != $scope.registerModel.ConfirmPassword) {
+		if (($scope.registerModel.Password &&
+            $scope.registerModel.ConfirmPassword) &&
+            $scope.registerModel.Password != $scope.registerModel.ConfirmPassword) {
 			$scope.registerForm.confirmPasswordInput.$setValidity("match", false);
 		}
 	}
 
 	$scope.register = function() {
-		$scope.registerFailed = false;
+        if (!$scope.registerForm.$valid)
+            return;
+
+        $scope.registerErrors = {};
+        let queryString = 'mutation{register(username:"' + $scope.registerModel.Username + '",password:"' + $scope.registerModel.Password + '")}';
+
 		$http({
-			url: '/php/login/register.php',
+            url: "/api",
 			method: 'POST',
-			data: $scope.registerModel
+            data: { query: queryString }
 		}).then(function succcessCallback(response) {
-			if (response.data.success) {
+            if (response.data.data) {
 				$('#loginCollapse').collapse('show');
 				$('#registerCollapse').collapse('hide');
-			}
-			else {
-				$scope.registerFailed = true;
-			}
+            }
 		}, function errorCallback(response){
-
+            if (response.data.errors) {
+                $scope.registerErrors = response.data.errors;
+            }
 		});
-	}
+	};
 
 	$scope.login = function() {
-		$scope.loginMessages = {};
+        $scope.loginErrors = {};
+        let stayLoggedIn = !!$scope.loginModel.StayLoggedIn;
 
         let queryString =
-            '{authLogin(username:"' + $scope.loginModel.Username +
-            '",Password:"' + $scope.loginModel.Password +
-            '",stayLoggedIn:' + $scope.loginModel.StayLoggedIn + ')}';
+            'mutation{authLogin(username:"' + $scope.loginModel.Username +
+            '",password:"' + $scope.loginModel.Password +
+            '",stayLoggedIn:' + stayLoggedIn + ') {' +
+            'token\n' +
+            'expires' +
+            '}}';
 
 		$http({
 			url: '/api',
 			method: 'POST',
 			data: { query: queryString }
 		}).then(function succcessCallback(response) {
-            if (response.data.errors) {
-                $scope.loginMessages = response.data.errors;
-            }
-            else if (response.data.data) {
+            if (response.data.data) {
                 let data = response.data.data;
-                if (data.authLogin.token && $cookies.get("cookie-consent"))
-                    $cookies.put("loginToken", data.authLogin.token, {"path": "/", "expires": data.authLogin.expires});
+                if (data.authLogin.token && $cookies.get("cookie-consent")) {
+                    let cookieData = {"path": "/", "samesite": "strict", "secure": true};
+                    if (stayLoggedIn)
+                        cookieData["expires"] = data.authLogin.expires;
+
+                    $cookies.put("loginToken", data.authLogin.token, cookieData);
+                }
 
                 let returnUrl = getUrlParameter("returnUrl");
                 if (returnUrl)
@@ -84,8 +71,12 @@ app.controller('login', ['$scope', '$cookies', '$http', function($scope, $cookie
                 else
                     window.location = "/";
             }
-		})
-	}
+        }, function errorCallback(response) {
+            if (response.data.errors) {
+                $scope.loginErrors = response.data.errors;
+            }
+        });
+	};
 
     initialize();
 }]);
--- www_new/routes/index.js
+++ www_new/routes/index.js
@@ -1,8 +1,12 @@
-var express = require("express");
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
 
 router.get(["/", "index.html"], function(req, res, next) {
   res.render("index", {});
+});
+
+router.get(["/login.html"], function(req, res, next) {
+    res.render("login", {});
 });
 
 module.exports = router;
