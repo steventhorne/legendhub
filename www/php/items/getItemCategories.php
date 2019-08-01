cc49cf2f6f14c399709f17ad3a9f793deb2e14fc
SvarturH <shalldursson@gmail.com>
2019-08-01 11:11:33 +0000

    Removed old www directory

--- www/php/items/getItemCategories.php
+++
@@ -1,9 +0,0 @@
-<?php
-header( "Access-Control-Allow-Origin: legendhub.org" );
-header( "Content-Type: application/json; charset=UTF-8" );
-
-$root = realpath(getenv("DOCUMENT_ROOT"));
-require_once("$root/php/common/itemConstants.php");
-
-echo(json_encode($itemCategories));
-?>
