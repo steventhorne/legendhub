4cc04e4bf2c033ea08a90b10bb013680c3735f77
steventhorne <steven.thorne91@gmail.com>
2020-01-26 21:30:31 -0500

    Restructured project and added initial docker-compose/Dockerfile for mysql and www

--- www/routes/api/mysql-connection.js
+++ www/routes/api/mysql-connection.js
@@ -2,10 +2,11 @@ let mysql = require("mysql")
 
 let pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.HOST,
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 module.exports = pool;
