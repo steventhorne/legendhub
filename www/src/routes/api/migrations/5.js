615b10a261ea7be6c6eb6c5b6a19b8c700e3b49e
steventhorne <steven.thorne91@gmail.com>
2023-05-30 16:13:47 -0400

    Another migration fix

--- www/src/routes/api/migrations/5.js
+++ www/src/routes/api/migrations/5.js
@@ -2,7 +2,15 @@ exports.up = function() {
     return `
         -- Update SortNumber to split up category 1 again
         UPDATE ItemStatInfo
-        SET SortNumber = SortNumber + 895
+        SET SortNumber = SortNumber + 100
+        WHERE SortNumber >= 300;
+
+        UPDATE ItemStatInfo
+        SET SortNumber = 300
+        WHERE Id = 12;
+
+        UPDATE ItemStatInfo
+        SET SortNumber = SortNumber + 995
         WHERE SortNumber >= 105 AND SortNumber <= 114;
     `;
 }
@@ -11,7 +19,15 @@ exports.down = function() {
     return `
         -- Update SortNumber to merge category 1 again
         UPDATE ItemStatInfo
-        SET SortNumber = SortNumber - 895
-        WHERE SortNumber >= 1000 AND SortNumber <= 1009;
+        SET SortNumber = SortNumber - 995
+        WHERE SortNumber >= 1100 AND SortNumber <= 1109;
+
+        UPDATE ItemStatInfo
+        SET SortNumber = 104
+        WHERE Id = 12;
+
+        UPDATE ItemStatInfo
+        SET SortNumber = SortNumber - 100
+        WHERE SortNumber >= 400;
     `;
 }
